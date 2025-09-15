b514d729ce5ba2310342fbd79533d42cf7569bfe
Jack Yu <jack.yu@suse.com>
2025-09-15 17:36:07 +0800

    feat: fetch specific node annotation to install nvidia driver
    Signed-off-by: Jack Yu <jack.yu@suse.com>

--- nvidia-driver-toolkit/entrypoint.sh
+++ nvidia-driver-toolkit/entrypoint.sh
@@ -18,16 +18,38 @@ create_dev_char_directory() {
 	fi
 }
 
+install_nvidia_driver() {
+    local driver_location="$1"
+    
+    if [ -n "$driver_location" ]; then
+        echo "Installing nvidia driver from $driver_location"
+        curl -o /tmp/NVIDIA.run -k "$driver_location"
+        chmod +x /tmp/NVIDIA.run
+        /tmp/NVIDIA.run -q --ui=none --no-systemd
+    else
+        echo "No driver location specified. Skipping driver installation..."
+    fi
+}
 
-if [ -n "${DRIVER_LOCATION}" ]
-then
-    echo "Installing nvidia driver from ${DRIVER_LOCATION}"
-    curl -o /tmp/NVIDIA.run -k $DRIVER_LOCATION
-    chmod +x /tmp/NVIDIA.run
-    /tmp/NVIDIA.run -q --ui=none --no-systemd
-else
-    echo "No DRIVER_LOCATION specified. skipping..."
-fi
+get_nvidia_driver_version() {
+  local driver_location="$DRIVER_LOCATION"  # Default driver location
+  local annotation_key="sriovgpu.harvesterhci.io/custom-driver"
+  local node_name="$NODE_NAME"
+  local custom_driver=$(kubectl get node "$node_name" -o json | jq -r ".metadata.annotations[\"$annotation_key\"] // empty")
+
+  if [ -n "$custom_driver" ]; then
+    echo >&2 "Found custom driver annotation: $annotation_key=$custom_driver"
+    driver_location="$custom_driver"
+  else
+    echo >&2 "No custom driver annotation found. Using default driver location."
+  fi
+
+  echo $driver_location
+}
+
+driver_location=$(get_nvidia_driver_version)
+echo "Driver location: $driver_location"
+install_nvidia_driver "$driver_location"
 
 echo "running nvidia vgpud"
 create_dev_char_directory
